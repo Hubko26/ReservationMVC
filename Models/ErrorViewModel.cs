c4789374a98860b08c7ad1b1d4098532de0455e5
Hubko26 <85346374+Hubko26@users.noreply.github.com>
2021-06-04 14:39:52 +0200

    Initial push
    Initial push with CSS and JS

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReservationMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
