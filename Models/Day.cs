cc439fff9476d97a1c4b1446097199edda9587eb
Hubko26 <85346374+Hubko26@users.noreply.github.com>
2021-06-04 16:53:31 +0200

    Initial classes created

---
+++ Models/Day.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReservationMVC.Enum;
+
+namespace ReservationMVC.Models
+{
+    public class Day
+    {
+        bool isValidDate;
+        Days dayName;
+        List<Dictionary<int, bool>> hourOccupancy = new List<Dictionary<int, bool>>();
+
+        public Day(List<Dictionary<int, bool>> hourOccupancy, Days dayName, bool isWorkingDay)
+        {
+            HourOccupancy = hourOccupancy;
+            DayName = dayName;
+            IsWorkingDay = isWorkingDay;
+        }
+
+        public List<Dictionary<int, bool>> HourOccupancy { get => hourOccupancy; set => hourOccupancy = value; }
+        public Days DayName { get => dayName; set => dayName = value; }
+        public bool IsWorkingDay { get => isValidDate; set => isValidDate = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Day day &&
+                   EqualityComparer<List<Dictionary<int, bool>>>.Default.Equals(HourOccupancy, day.HourOccupancy) &&
+                   DayName == day.DayName &&
+                   isValidDate == day.isValidDate;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(HourOccupancy, DayName, IsWorkingDay);
+        }
+    }
+}
---
+++ Models/Month.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ReservationMVC.Enum;
+
+namespace ReservationMVC.Models
+{
+    public class Month
+    {
+        int numberOfDays;
+        Days firstDayOfMonth;
+
+        public Month(int numberOfDays, Days firstDayOfMonth)
+        {
+            this.numberOfDays = numberOfDays;
+            this.FirstDayOfMonth = firstDayOfMonth;
+        }
+
+        public Days FirstDayOfMonth { get => firstDayOfMonth; set => firstDayOfMonth = value; }
+        public int NumberOfDays { get => numberOfDays; set => numberOfDays = value; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Month month &&
+                   FirstDayOfMonth == month.FirstDayOfMonth &&
+                   NumberOfDays == month.NumberOfDays;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FirstDayOfMonth, NumberOfDays);
+        }
+    }
+}
